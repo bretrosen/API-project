c05343e246433f239d03deb043cab8f38130bdaf
Bret Rosen <bretrosen@gmail.com>
2023-04-12 16:45:08 -0500

    added an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+// connect api router
+router.use('/api', apiRouter);
 
 // allow easy retrieval of XSRF token
 router.get("/api/csrf/restore", (req, res) => {
