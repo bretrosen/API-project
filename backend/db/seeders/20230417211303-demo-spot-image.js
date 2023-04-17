fac2e603e14fb8edc0d37573447edeb8e8cd5d66
Bret Rosen <bretrosen@gmail.com>
2023-04-17 17:43:16 -0500

    added spot-image and booking seeds

--- backend/db/seeders/20230417211303-demo-spot-image.js
+++ backend/db/seeders/20230417211303-demo-spot-image.js
@@ -18,6 +18,34 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+   options.tableName = 'SpotImages';
+   await queryInterface.bulkInsert(options, [
+    {
+      spotId: 1,
+      url: "https://fugazzi.com",
+      preview: true
+    },
+    {
+      spotId: 1,
+      url: "https://fugazzi.com",
+      preview: false
+    },
+    {
+      spotId: 2,
+      url: "https://sherlock.com",
+      preview: true
+    },
+    {
+      spotId: 3,
+      url: "https://toilets.com",
+      preview: false
+    },
+    {
+      spotId: 4,
+      url: "https://cows.com",
+      preview: true
+    }
+   ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -27,5 +55,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3, 4]}
+    }, {});
   }
 };
--- backend/db/seeders/20230417211318-demo-booking.js
+++ backend/db/seeders/20230417211318-demo-booking.js
@@ -1,8 +1,14 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -12,14 +18,46 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    options.tableName = 'Bookings';
+    await queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 4,
+        startDate: 12 / 25 / 23,
+        endDate: 12 / 29 / 23
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate: 1 / 2 / 24,
+        endDate: 1 / 29 / 24
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        startDate: 1 / 25 / 24,
+        endDate: 1 / 28 / 24
+      },
+      {
+        spotId: 4,
+        userId: 1,
+        startDate: 2 / 12 / 24,
+        endDate: 2 / 29 / 24
+      },
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3, 4] }
+    }, {});
   }
 };
