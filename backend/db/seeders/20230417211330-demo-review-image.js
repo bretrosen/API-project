54c8911a1ae90599af783807aca0d26454817791
Bret Rosen <bretrosen@gmail.com>
2023-04-17 18:38:40 -0500

    added seeds for ReviewImages

--- backend/db/seeders/20230417211330-demo-review-image.js
+++ backend/db/seeders/20230417211330-demo-review-image.js
@@ -1,6 +1,12 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +18,33 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+   options.tableName = 'ReviewImages';
+   await queryInterface.bulkInsert(options, [
+    {
+      reviewId: 1,
+      url: 'https://googlify.com/hamsters'
+    },
+    {
+      reviewId: 2,
+      url: 'https://googlify.com/houses'
+    },
+    {
+      reviewId: 3,
+      url: 'https://googlify.com/hotels'
+    },
+    {
+      reviewId: 4,
+      url: 'https://googlify.com/horses'
+    },
+    {
+      reviewId: 5,
+      url: 'https://googlify.com/hansoms'
+    },
+    {
+      reviewId: 6,
+      url: 'https://googlify.com/hotplates'
+    },
+   ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +54,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: [1, 2, 3, 4, 5, 6] }
+    }, {});
   }
 };
