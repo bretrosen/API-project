812c3681e2ca00264165b18403c0de76cd19f3ce
Bret Rosen <bretrosen@gmail.com>
2023-04-17 20:25:22 -0500

    added very basic functionality to get all Spots

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {Spot, Review, ReviewImage } = require('../../db/models');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    const spots = Spot.findAll();
+
+    return res.json(spots);
+})
+
+module.exports = router;
