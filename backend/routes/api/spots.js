abf04563565d4da9924da4976bdbf2e930cd560b
Bret Rosen <bretrosen@gmail.com>
2023-04-17 21:29:48 -0500

    got aggregate data for all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const {Spot, Review, ReviewImage } = require('../../db/models');
+const {Spot, Review, SpotImage } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    const spots = await Spot.findAll();
+    const spots = await Spot.findAll({
+    });
 
-    return res.json(spots);
+    const spotsWithAvgRating = [];
+
+    for (let i = 0; i < spots.length; i++) {
+        const spotObj = spots[i].toJSON();
+        const reviewCount = await Review.count({
+            where: {spotId: spotObj.id}
+        });
+        const reviewSum = await Review.sum('stars', {
+            where: {spotId: spotObj.id}
+        });
+        const avgRating = (reviewSum / reviewCount).toFixed(1);
+        spotObj.avgRating = parseFloat(avgRating);
+        spotsWithAvgRating.push(spotObj);
+
+        const imageUrl = await SpotImage.findByPk({
+            attributes: ['url'],
+            where: {}
+        })
+    }
+
+    // console.log(spotsWithAvgRating);
+
+    return res.json({'Spots': spotsWithAvgRating});
 })
 
 module.exports = router;
