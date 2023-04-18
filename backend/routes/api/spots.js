963335eca66de63a9c8f00c4b423669dea332dc9
Bret Rosen <bretrosen@gmail.com>
2023-04-18 17:01:45 -0500

    added express-validator middleware for validating new spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,12 +6,42 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+// validate fields for spot on creating spot
 const validateSpot = [
-    check('ownerId')
+    check('address')
+        .exists({checkFalsy: true})
+        .withMessage("Street address is required"),
+    check('city')
         .exists({ checkFalsy: true})
-        .withMessage("Please provide an owner ID.")
-
-]
+        .withMessage("City is required"),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage("State is required"),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage("Country is required"),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .isLength({ max: 10})
+        .withMessage("Latitude is not valid"),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .isLength({ max: 10})
+        .withMessage("Longitude is not valid"),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50})
+        .withMessage("Name must be less than 50 characters"),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+];
 
 // helper function to return spot data including average rating and preview image
 const returnSpotData = async (spots) => {
@@ -138,7 +168,7 @@ router.get('/', async (_req, res) => {
 
 
 // create a new spot
-router.post('/', requireAuth, async (req, res, next) => {
+router.post('/', requireAuth, validateSpot, async (req, res, next) => {
     const { ownerId, address, city, state, country, lat, lng, name, description, price } = req.body;
 
     const newSpot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price});
