56f9c15265094d74d63274026d824c698039a765
Bret Rosen <bretrosen@gmail.com>
2023-04-19 16:16:27 -0500

    working on user-reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -6,6 +6,46 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+// get all reviews of the current user
+router.get('/current', requireAuth, async (req, res, next) => {
+    const { user } = req;
 
+    const reviewArray = [];
+    const allReviewsObj = {};
+
+    const reviews = await Review.findAll({
+        where: {userId: user.id}
+    });
+
+    for (let i = 0; i < reviews.length; i++) {
+        const reviewObj = reviews[i].toJSON();
+
+        const user = await User.findByPk(reviewObj.userId, {
+            attributes: ['id', 'firstName', 'lastName']
+        });
+        const jsonUser = user.toJSON();
+        console.log(jsonUser);
+        reviewObj.User = jsonUser;
+
+        reviewArray.push(reviewObj);
+
+        const spot = await Spot.findByPk(reviewObj.spotId, {
+            attributes: [['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price']]
+        });
+        spotObj = spot.toJSON();
+
+        const previewImage = 
+
+        reviewArray.push(spotObj);
+
+
+
+    }
+    allReviewsObj.Reviews = reviewArray;
+    // console.log(allReviewsObj);
+
+    return res.json(allReviewsObj);
+
+})
 
 module.exports = router;
