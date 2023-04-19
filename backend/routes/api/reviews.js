d5749c06ab4936181ad41fb4b60d21fc54cba269
Bret Rosen <bretrosen@gmail.com>
2023-04-19 14:56:48 -0500

    added reviews router

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { User, Spot, Review, SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+
+
+module.exports = router;
