b048d1257af4aa3f833f69f2ccea8b6012977eda
Bret Rosen <bretrosen@gmail.com>
2023-04-20 19:06:06 -0500

    working on deleting spot image

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -6,5 +6,19 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+    const spotImage = await SpotImage.findByPk(req.params.imageId);
+    const { user } = req;
+
+    // error response for invalid spot image
+    if (!spotImage) {
+        res.status(404);
+        return res.json({
+            message: "Spot Image couldn't be found"
+        });
+    }
+
+    // error response for unauthorized user
+})
 
 module.exports = router;
