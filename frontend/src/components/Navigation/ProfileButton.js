2e6970d1099bde2cfd5bf929eca73597622ba2d3
Bret Rosen <bretrosen@gmail.com>
2023-05-05 16:02:10 -0500

    added dropdown menu for user profile button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import './Navigation.css';
+
 
 const ProfileButton = ({ user }) => {
     const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) {
+            setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
 
     const logout = (event) => {
         event.preventDefault();
         dispatch(sessionActions.logoutThunk());
     };
 
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
     return (
         <>
-            <button>
+            <button onClick={openMenu}>
                 <i className="fa-solid fa-book-atlas" />
             </button>
-            <ul className="profile-dropdown">
+            <ul className={ulClassName} ref={ulRef}>
                 <li>{user.username}</li>
                 <li>{user.firstName} {user.lastName}</li>
                 <li>{user.email}</li>
