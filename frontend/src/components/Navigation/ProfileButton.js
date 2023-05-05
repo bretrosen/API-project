fbfba90d76dccf1427bd750cc0d4a0c68ad3ebfd
Bret Rosen <bretrosen@gmail.com>
2023-05-05 15:41:47 -0500

    added profile button with user details

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,30 @@
-const ProfileButton = () => {
+import React from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import './Navigation.css';
+
+const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch();
+
+    const logout = (event) => {
+        event.preventDefault();
+        dispatch(sessionActions.logoutThunk());
+    };
+
     return (
-        <div style={{ color: "purple", fontSize: "100px" }}>
-            {/* <FontAwesomeIcon icon="fa-solid fa-book-atlas" /> */}
-            <i className="fa-solid fa-book-atlas"></i>
-        </div>
+        <>
+            <button>
+                <i className="fa-solid fa-book-atlas" />
+            </button>
+            <ul className="profile-dropdown">
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </>
     );
 }
 
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import * as sessionActions from '../../store/session';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({isLoaded}) {
     const sessionUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch();
-
-    const logout = (event) => {
-        event.preventDefault();
-        dispatch(sessionActions.logoutThunk());
-    };
 
     let sessionLinks;
     if (sessionUser) {
         sessionLinks = (
             <li>
                 <ProfileButton user={sessionUser} />
-                <button onClick={logout}>Log Out</button>
             </li>
         );
     } else {
