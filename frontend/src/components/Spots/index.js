9a571cb043bacbeddd9700a1aff787516e1aabee
Bret Rosen <bretrosen@gmail.com>
2023-05-08 13:02:48 -0500

    added functionality for listing all spots

---
+++ frontend/src/components/Spots/index.js
@@ -0,0 +1,64 @@
+// get all spots index component
+
+// backend route to get all spots ('/api/spots')
+// backend responds with
+// {
+//     'Spots': spotData,
+//     "page": page,
+//     "size": size
+// }
+
+// redux
+// type string
+// action creator
+// thunk action creator
+// reducer: case in the reducer for getting all spots
+// normalize spot data
+
+// component
+// dispatch to fetch data and add to store
+// useSelector gets data from store and gives component access to data
+// useEffect triggers dispatch of thunk
+// convert spotData obj to list
+// show data on page through return jsx, map through list of spots
+
+// render component in app.js, import into app.js
+// route, path
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getAllSpotsThunk } from '../../store/spots';
+import './SpotList.css';
+
+export const SpotList = () => {
+    const dispatch = useDispatch();
+    // get spots from store
+    const spots = useSelector(state => (Object.values(state.spots.allSpots)));
+
+    // useEffect to trigger dispatch of thunk
+    useEffect(() => {
+        dispatch(getAllSpotsThunk());
+    }, [dispatch]);
+
+    return (
+        <>
+            <h1>Spots Index</h1>
+            <ul>
+                {spots.map((spot) => (
+                    <>
+                    <div key={spot.id}>
+                        imageUrl: {spot.previewImage}
+                        <img src={spot.previewImage} alt={spot.name}></img>
+                    </div>
+                    <p key={spot.id}>
+                        {spot.city}, {spot.state}
+                    </p>
+                    <p key={spot.id}>
+                        ${spot.price} night
+                        </p>
+                    </>
+    ))}
+            </ul>
+        </>
+    );
+};
